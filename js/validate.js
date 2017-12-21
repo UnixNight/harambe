5336b81ebb898d81bb67006c8eae7cf8d164800a
Megaman1811 <cristian.cerroni@gmail.com>
2017-12-21 00:02:34 -0500

    Finished phase 2

--- js/validate.js
+++ js/validate.js
@@ -1,16 +1,42 @@
-// Validate.js
-// Cristian Cerroni 2017
-
 function validate() {
     var fName = document.getElementById("firstname").value;
     var lName = document.getElementById("lastname").value;
+    var phone = document.getElementById("phoneNum").value;
     var email = document.getElementById("email").value;
-    var regEmail = /^[a-zA-Z0-9_\-\.]+@+[a-z0-9_\-\.]+\.[a-z.]{2,4}$/;
+    var emailTest = /^[a-zA-Z0-9_\-\.]+@+[a-z0-9_\-\.]+\.[a-z.]{2,4}$/;
+    var comment = document.getElementById("comment").value;
+
+
 
     var rMale = document.getElementById("rMale").checked;
     var rFemale = document.getElementById("rFemale").checked;
     var rOther = document.getElementById("rOther").checked;
     var rList = document.getElementsByName("rGender");
+
+    var check1 = document.getElementById("check1").checked;
+    var check2 = document.getElementById("check2").checked;
+
+
+
+    if (fName == "" || fName == null) {
+        alert("Please enter first name.");
+        return false;
+    }
+
+    if (lName == "" || lName == null) {
+        alert("Please enter last name.");
+        return false;
+    }
+
+    if (emailTest.test(email) == false) {
+        alert("Please enter an email address.");
+        return false;
+    }
+
+    if (phone == "" || phone == null || phone.length < 10 || phone.length > 10) {
+        alert("Please enter a valid phone number.");
+        return false;
+    }
 
     for (i = 0; i < rList.length; i++) {
         if (rList[i].checked == true) {
@@ -20,15 +46,22 @@ function validate() {
 
     if (rMale == false && rFemale == false && rOther == false) {
         alert("Please select a gender");
-    }
-
-    if (name == "" || name == null) {
-        alert("Please enter a value in the name field.");
         return false;
     }
 
-    if (regEmail.test(email) == false) {
-        alert("Please enter a valid email address.");
+    if (comment == "" || comment == null) {
+        alert("Please enter a comment.");
+        return false;
     }
-}
 
+    if (check1 == false && check2 == false) {
+        alert("Please check one of the boxes for preferred contact");
+        return false;
+    }
+
+
+    alert(fName +"\n" + lName+ "\n"+ phone+ "\n" + email + "\n" + comment );
+
+
+    // im not doing my other form
+}
